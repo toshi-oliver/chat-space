a541fab2274e74c09d0b9faa7a5c886a8140739f
toshi-oliver <igpoid@gmail.com>
2018-05-30 20:09:30 +0900

    Edit-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  root  'messages#index'
+  # root to 'messages#index'
+  # resources :messages, only: [:index]
+>>>>>>> c4f0855... edit view page
 end
