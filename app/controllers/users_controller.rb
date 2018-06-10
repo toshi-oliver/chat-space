4bb866a5bd368d17a13b250f0f3624cf9f405011
toshi-oliver <igpoid@gmail.com>
2018-06-10 15:55:33 +0900

    edit the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
   end
 end
-
-
