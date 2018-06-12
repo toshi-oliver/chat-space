c616e5e6ab1e0b2cd0ac24663631f59dd8274269
toshi-oliver <igpoid@gmail.com>
2018-06-12 13:53:06 +0900

    Create the auto_update.js

---
+++ app/assets/javascripts/auto_update.js
@@ -0,0 +1,58 @@
+$(function() {
+  function buildHTML(message){
+    if (message.image) {
+      var image = `<img src="${message.image}">`;
+    } else {
+      var image = '';
+    }
+    var html = `
+      <div class="main__body__messages" data-id="${message.id}">
+        <div class="main__body__user-name">
+          ${message.user_name}
+        </div>
+        <div class="main__body__posted-time">
+          ${message.created_at}
+        </div>
+        <div class="main__body__content">
+          <div class="content__text">
+            ${message.content}
+          </div>
+          <div class="content__image">
+            ${image}
+          </div>
+        </div>
+      </div>
+    `
+    return html;
+  };
+
+  function buildhtml(input){
+    $('.main__body').append(input)
+    $('#new_message')[0].reset()
+    $('.main__body').animate({scrollTop:$('.main__body')[0].scrollHeight}, 'swing');
+  };
+
+  setInterval(update, 5000);
+
+  function update() {
+    var message_id = $('.main__body__messages:last').data('id');
+        console.log(message_id)
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: {id: message_id},
+      dataType: 'json'
+    })
+    .done(function(data) {
+      if (data.length !== 0){
+      data.forEach(function(message){
+        var html = buildHTML(message);
+      buildhtml(html);
+    });
+    }
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    })
+  };
+});
