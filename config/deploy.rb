7a10dfb5803c234e834e5df31dedfa8134d81772
toshi-oliver <igpoid@gmail.com>
2018-06-17 18:12:05 +0900

    edit config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '3.1'
 
 set :application, 'chat-space'
 set :repo_url,  'git@github.com:toshi-oliver/chat-space.git'
